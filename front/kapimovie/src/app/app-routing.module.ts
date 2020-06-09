f1b2b4535ae65aecfecbf9624bb4e7ef845d0f96
Joao Pedro <joao.sjc12@hotmail.com>
2020-06-08 21:04:31 -0300

    settings routers

--- front/kapimovie/src/app/app-routing.module.ts
+++ front/kapimovie/src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProfileComponent } from './views/user/profile/profile.component';
+import { MovieListAllComponent } from './views/movie-components/movie-list-all/movie-list-all.component';
+import { MovieListFavoriteComponent } from './views/movie-components/movie-list-favorite/movie-list-favorite.component';
+import { MovieDetailsComponent } from './views/movie-components/movie-details/movie-details.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'profile', pathMatch: 'full' },
+  { path: 'profile', component: ProfileComponent},
+  { path: 'movies', component: MovieListAllComponent},
+  { path: 'movies-liked', component: MovieListFavoriteComponent},
+  { path: 'movie/:id', component: MovieDetailsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
